bab94c8de617aa5b98ba05da19ea60a4d8509260
Jonathan Zernik <yzernik@gmail.com>
2018-08-28 00:38:16 -0700

    Add Messages (#4)
    * Add base class for messages, and add some message classes.
    
    * Remove newline

---
+++ src/squeak/net.py
@@ -0,0 +1,63 @@
+import struct
+
+from bitcoin.core.serialize import Serializable
+from bitcoin.core.serialize import VectorSerializer
+from bitcoin.core.serialize import ser_read
+from bitcoin.core import b2lx
+
+from squeak.core import PUB_KEY_LENGTH
+
+PROTO_VERSION = 1
+
+
+class CSqueakLocator(Serializable):
+    """Used for locating desired squeaks.
+
+    Contains a list of signing keys, each with a block height range.
+    """
+    def __init__(self, protover=PROTO_VERSION):
+        self.nVersion = protover
+        self.vInterested = []
+
+    @classmethod
+    def stream_deserialize(cls, f):
+        c = cls()
+        c.nVersion = struct.unpack(b"<i", ser_read(f, 4))[0]
+        c.vInterested = VectorSerializer.stream_deserialize(CInterested, f)
+        return c
+
+    def stream_serialize(self, f):
+        f.write(struct.pack(b"<i", self.nVersion))
+        VectorSerializer.stream_serialize(CInterested, self.vInterested, f)
+
+    def __repr__(self):
+        return "CSqueakLocator(nVersion=%i vInterested=%s)" % \
+            (self.nVersion, repr(self.vInterested))
+
+
+class CInterested(Serializable):
+    """Contains a signing key together with a block range.
+
+    """
+    def __init__(self, protover=PROTO_VERSION):
+        self.vchPubkey = b'\x00' * PUB_KEY_LENGTH
+        self.nMinBlockHeight = 0
+        self.nMaxBlockHeight = 0
+
+    @classmethod
+    def stream_deserialize(cls, f):
+        c = cls()
+        c.vchPubkey = ser_read(f,PUB_KEY_LENGTH)
+        c.nMinBlockHeight = struct.unpack(b"<i", ser_read(f,4))[0]
+        c.nMaxBlockHeight = struct.unpack(b"<i", ser_read(f,4))[0]
+        return c
+
+    def stream_serialize(self, f):
+        assert len(self.vchPubkey) == PUB_KEY_LENGTH
+        f.write(self.vchPubkey)
+        f.write(struct.pack(b"<I", self.nMinBlockHeight))
+        f.write(struct.pack(b"<I", self.nMaxBlockHeight))
+
+    def __repr__(self):
+        return "CInterested(vchPubkey=lx(%s) nMinBlockHeight=%s nMaxBlockHeight=%s)" % \
+            (b2lx(self.vchPubkey), repr(self.nMinBlockHeight), repr(self.nMaxBlockHeight))
--- tests/test_messages.py
+++ tests/test_messages.py
@@ -1,6 +1,11 @@
 import unittest
 
+from io import BytesIO
+
+from squeak.messages import msg_getheaders
 from squeak.messages import msg_version
+from squeak.messages import msg_verack
+from squeak.messages import SqueakMsgSerializable
 
 
 class MessageTestCase(unittest.TestCase):
@@ -19,3 +24,30 @@ class Test_msg_version(MessageTestCase):
     def test_user_agent(self):
         m = msg_version()
         self.assertTrue(b'squeak' in m.strSubVer)
+
+
+class Test_msg_getheaders(MessageTestCase):
+    def test_serialization(self):
+        super(Test_msg_getheaders, self).serialization_test(msg_getheaders)
+
+
+class Test_messages(unittest.TestCase):
+    verackbytes = b'\xf9\xbe\xb4\xd9verack\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00]\xf6\xe0\xe2'
+
+    def test_read_msg_verack(self):
+        f = BytesIO(self.verackbytes)
+        m = SqueakMsgSerializable.stream_deserialize(f)
+        self.assertEqual(m.command, msg_verack.command)
+        print(m)
+        self.assertTrue(isinstance(m, SqueakMsgSerializable))
+
+    def test_fail_invalid_message(self):
+        bad_verack_bytes = b'\xf8' + self.verackbytes[1:]
+        f = BytesIO(bad_verack_bytes)
+        with self.assertRaises(ValueError):
+            SqueakMsgSerializable.stream_deserialize(f)
+
+    def test_msg_verack_to_bytes(self):
+        m = msg_verack()
+        b = m.to_bytes()
+        self.assertEqual(self.verackbytes, b)
