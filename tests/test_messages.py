3b7315ea902622edbe2eb90ae33f682e67fc1d54
Jonathan Zernik <jzernik@jonathans-mbp.lan>
2018-08-28 19:30:21 -0700

    Implement remaining messages.

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -17,6 +17,9 @@ from squeak.messages import msg_getaddr
 from squeak.messages import msg_ping
 from squeak.messages import msg_pong
 from squeak.messages import msg_reject
+from squeak.messages import msg_offer
+from squeak.messages import msg_acceptoffer
+from squeak.messages import msg_invoice
 
 
 class MessageTestCase(unittest.TestCase):
@@ -27,6 +30,7 @@ class MessageTestCase(unittest.TestCase):
         mSerialzedTwice = mDeserialzed.to_bytes()
         self.assertEqual(mSerialized, mSerialzedTwice)
         self.assertTrue(isinstance(mDeserialzed, MsgSerializable))
+        self.assertTrue(isinstance(str(mDeserialzed), str))
 
 
 class Test_msg_version(MessageTestCase):
@@ -101,6 +105,21 @@ class Test_msg_pong(MessageTestCase):
 class Test_msg_reject(MessageTestCase):
     def test_serialization(self):
         super(Test_msg_reject, self).serialization_test(msg_reject)
+
+
+class Test_msg_offer(MessageTestCase):
+    def test_serialization(self):
+        super(Test_msg_offer, self).serialization_test(msg_offer)
+
+
+class Test_msg_acceptoffer(MessageTestCase):
+    def test_serialization(self):
+        super(Test_msg_acceptoffer, self).serialization_test(msg_acceptoffer)
+
+
+class Test_msg_invoice(MessageTestCase):
+    def test_serialization(self):
+        super(Test_msg_invoice, self).serialization_test(msg_invoice)
 
 
 class Test_messages(unittest.TestCase):
