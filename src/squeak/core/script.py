a7bf272dd739f48108e4201bc592977e40a636b4
Jonathan Zernik <yzernik@gmail.com>
2018-09-17 19:04:36 -0700

    Refactor script imports with module override.

--- src/squeak/core/script.py
+++ src/squeak/core/script.py
@@ -1,15 +1,6 @@
-import bitcoin.core.script
-
-
-def _new_RawSignatureHash(script, hash, _, hashtype):
-    return (hash, None)
-
-
-bitcoin.core.script.RawSignatureHash = _new_RawSignatureHash
-
 from bitcoin.core.script import CScript as BitcoinCScript
-from bitcoin.core.scripteval import VerifyScript as BitcoinVerifyScript
-from bitcoin.core.scripteval import VerifyScriptError
+
+from squeak.core._script import _VerifyScript
 
 
 class CScript(BitcoinCScript):
@@ -17,8 +8,4 @@ class CScript(BitcoinCScript):
 
 
 def VerifyScript(scriptSig, scriptPubKey, hash):
-    try:
-        BitcoinVerifyScript(scriptSig, scriptPubKey, hash, 0)
-        return True
-    except VerifyScriptError:
-        return False
+    return _VerifyScript(scriptSig, scriptPubKey, hash)
