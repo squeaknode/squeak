8bb51daf79273c5ed18419f3a796be085556e147
Jonathan Zernik <yzernik@gmail.com>
2018-09-17 19:07:38 -0700

    Add make sigscript function.

--- src/squeak/core/script.py
+++ src/squeak/core/script.py
@@ -3,9 +3,20 @@ from bitcoin.core.script import CScript as BitcoinCScript
 from squeak.core._script import _VerifyScript
 
 
+SIGHASH_ALL = b'\01'
+
+
 class CScript(BitcoinCScript):
     pass
 
 
 def VerifyScript(scriptSig, scriptPubKey, hash):
     return _VerifyScript(scriptSig, scriptPubKey, hash)
+
+
+def MakeSigScript(signature, verifying_key):
+    pubkey_bytes = verifying_key.serialize()
+    script = CScript()
+    script += (signature + SIGHASH_ALL)
+    script += pubkey_bytes
+    return script
