e2e254842bc1a4dd5efd8803c0f752cb60ba5162
yzernik <yzernik@gmail.com>
2020-05-01 14:32:20 -0700

    Add test for serialization of all fields.

--- tests/core/test_deserialize.py
+++ tests/core/test_deserialize.py
@@ -35,3 +35,7 @@ class TestDeserializeSqueak(object):
         assert(deserialized_squeak.hashBlock == lx('4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b'))
         assert(deserialized_squeak.nBlockHeight == 0)
         assert(deserialized_squeak.scriptPubKey == CScript([OP_DUP, OP_HASH160, x('d90be698f3b2dd28eb8f32933186fda15e9cc5f3'), OP_EQUALVERIFY, OP_CHECKSIG]))
+        assert(deserialized_squeak.hashDataKey == lx('a892b040034ca5e70da84d7e5997653004df21de39e9db946692ebe7819a8f60'))
+        assert(deserialized_squeak.vchIv == lx('036516e4f1c0c55e1201e0a28f016ff3'))
+        assert(deserialized_squeak.nTime == 1588050767)
+        assert(deserialized_squeak.nNonce == 0x2885819d)
